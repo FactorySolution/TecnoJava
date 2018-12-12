06fe3ba6de971a16c27492e416ef2a97532402eb
André Luiz Oliveira <andre.npd@hotmail.com>
2018-12-11 23:03:07 -0200

    Inicio do Projeto

---
+++ src/main/java/br/com/tecnospeed/tecnojava/TecnoJavaApplication.java
@@ -0,0 +1,12 @@
+package br.com.tecnospeed.tecnojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnoJavaApplication.class, args);
+	}
+}
